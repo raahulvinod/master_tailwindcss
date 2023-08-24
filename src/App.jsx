7e741398baef27f8a1813edad49ce3a7ad355568
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-24 16:16:25 +0530

    Section Structure Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offers</section>
+    <section className="padding bg-pale-blue">CustomerReviews</section>
+    <section className="padding-x sm:py-16 w-full">Subscribe</section>
+    <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+  </main>
+);
+
+export default App;
