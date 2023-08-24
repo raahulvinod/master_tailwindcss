99e5e7ba2340a158ae84380bbdad2f218949c38d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-24 18:23:27 +0530

    Nav Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {
-  Nav,
   Hero,
   PopularProducts,
   SuperQuality,
@@ -9,10 +8,11 @@ import {
   Subscribe,
   Footer,
 } from './sections';
+import Nav from './components/Nav';
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import { nikepark } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={nikepark} alt="Logo" width={200} height={30} />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
