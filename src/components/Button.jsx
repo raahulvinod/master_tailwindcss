94f145bafac4bd8caeb801d6302d8979b60a7e3a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-27 23:38:54 +0530

    Super Quality Section Added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,7 +13,7 @@ const Button = ({
   ${
     backgroundColor
       ? `${backgroundColor} ${textColor} ${borderColor}`
-      : 'bg-orange-400 text-white border-coral-red'
+      : 'bg-orange-400 text-white border-orange-400'
   } rounded-full ${fullWidth && 'w-full'}`}
     >
       {label}
